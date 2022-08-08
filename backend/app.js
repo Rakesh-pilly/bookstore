870aac3706b805c5e63f19ae9839aa282d182a3f
rakesh-pilly <rakesh.mudhiraj882@gmail.com>
2022-08-08 20:03:36 +0530

    asod

--- backend/app.js
+++ backend/app.js
@@ -4,16 +4,21 @@ const router = require('./routes/book-routes');
 const app = express()
 const cors = require('cors')
 //Middleware 
+
+const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
+app.get("/",(req,res,next)=> {
+    res.status(200).json({message: "hello world"})
+})
 app.use("/books", router)
 
 
 mongoose.connect('mongodb+srv://rakesh:64tYtVQ0WtItOqyo@cluster0.u4ikm.mongodb.net/bookStore?retryWrites=true&w=majority')
 .then(()=> console.log("Connected to the database"))
 .then(()=> {
-    app.listen(5000,()=> {
-        console.log("port running on 5000")
+    app.listen(PORT,()=> {
+        console.log("port running on ", PORT)
     })
 }).catch((err)=> {
     console.log("error", err)
